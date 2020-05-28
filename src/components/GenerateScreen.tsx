e3ea1bf35d03e4c09825440d6d0b33dc9f714df7
Aubrey Powell <herufeanor@gmail.com>
2020-05-28 03:21:52 -0700

    Add gender selection to new character screen
    Also add some styling, to be more in line with the rest of the design.

--- src/components/GenerateScreen.tsx
+++ src/components/GenerateScreen.tsx
@@ -1,12 +1,33 @@
 import * as React from "react";
 import { State } from "../state/state";
-import { GenerateButton } from "./GenerateButton";
+import { generate } from "../generate";
+import { RaceName } from "../data/races";
 
 export function GenerateScreen(props: { state: State }) {
-	return <div className="flexrow" style={{ flex: 1, alignItems: "flex-start" }}>
-		<GenerateButton state={props.state} race="Human" />
-		<GenerateButton state={props.state} race="Elf" />
-		<GenerateButton state={props.state} race="Dwarf" />
-		<GenerateButton state={props.state} race="Halfling" />
+	let makeOnClick = (race: RaceName, gender: "Male" | "Female") => () => {
+		props.state.characters.push(generate(race, gender));
+		props.state.dirty = true;
+	}
+	return <div className="flexrow generateScreen">
+		<div className="flexcol group">
+			<div>Human</div>
+			<div className="button" onClick={makeOnClick("Human", "Male")}>Male</div>
+			<div className="button" onClick={makeOnClick("Human", "Female")}>Female</div>
+		</div>
+		<div className="flexcol group">
+			<div>Elf</div>
+			<div className="button" onClick={makeOnClick("Elf", "Male")}>Male</div>
+			<div className="button" onClick={makeOnClick("Elf", "Female")}>Female</div>
+		</div>
+		<div className="flexcol group">
+			<div>Dwarf</div>
+			<div className="button" onClick={makeOnClick("Dwarf", "Male")}>Male</div>
+			<div className="button" onClick={makeOnClick("Dwarf", "Female")}>Female</div>
+		</div>
+		<div className="flexcol group">
+			<div>Halfling</div>
+			<div className="button" onClick={makeOnClick("Halfling", "Male")}>Male</div>
+			<div className="button" onClick={makeOnClick("Halfling", "Female")}>Female</div>
+		</div>
 	</div>
 };
