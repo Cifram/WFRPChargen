c22cbc5aeabc278d752999c77ba9696a2ac6efc1
Aubrey Powell <herufeanor@gmail.com>
2020-06-02 01:03:22 -0700

    Increase the starting window size

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require("electron");
 function createWindow() {
 	// Create the browser window.
 	const win = new BrowserWindow({
-		width: 1600,
-		height: 600,
+		width: 1800,
+		height: 1000,
 		webPreferences: {
 			nodeIntegration: true,
 		},
