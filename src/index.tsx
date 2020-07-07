b25d0baca5fb83072cf7041d792be0d2eef04902
Aubrey Powell <herufeanor@gmail.com>
2020-07-07 03:29:49 -0700

    Add saving and loading of characters

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,43 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as fs from "fs";
 
 import { App } from "./components/App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { rootReducer } from "./store/reducers/rootReducer";
+import { Character } from "./store/state/character";
+import { addCharacter } from "./store/actions/AddChracter";
 
 const store = createStore(rootReducer);
+
+const charDirName = "./characters/";
+{
+	if (!fs.existsSync(charDirName)) {
+		fs.mkdirSync(charDirName);
+	}
+	const fileNames = fs.readdirSync(charDirName);
+	for (let fileName of fileNames) {
+		let json = fs.readFileSync(`${charDirName}${fileName}`, "utf8");
+		let char = JSON.parse(json) as Character;
+		store.dispatch(addCharacter(char));
+	}
+}
+
+const savedChars = store.getState().characters;
+
+function saveCharacter(char: Character) {
+	let json = JSON.stringify(char);
+	fs.writeFile(
+		`${charDirName}${char.name} - ${char.race} ${char.career}.json`,
+		json,
+		(err) => {
+			if (err != null) {
+				console.error(`Failed to write ${char.name} to file`);
+			}
+		}
+	);
+}
 
 function tick() {
 	ReactDOM.render(
@@ -15,6 +46,21 @@ function tick() {
 		</Provider>,
 		document.getElementById("root")
 	);
+
+	let state = store.getState();
+	if (state.characters != savedChars) {
+		if (state.characters.length > savedChars.length) {
+			saveCharacter(state.characters[state.characters.length - 1]);
+		} else {
+			for (let i = 0; i < state.characters.length; i++) {
+				if (state.characters[i] != savedChars[i]) {
+					saveCharacter(state.characters[i]);
+					break;
+				}
+			}
+		}
+	}
+
 	requestAnimationFrame(tick);
 }
 tick();
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
 
 	// Enable sourcemaps for debugging webpack's output.
 	devtool: "source-map",
-
+	target: "electron-renderer",
 	resolve: {
 		// Add '.ts' and '.tsx' as resolvable extensions.
 		extensions: [".ts", ".tsx", ".js"],
